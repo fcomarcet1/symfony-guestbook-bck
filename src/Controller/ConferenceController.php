6da8c0b99278e44b47c62f8f50ecd643e4bc8a80
Francisco Marcet <fcomarcet1@gmail.com>
2021-09-15 10:48:48 +0200

    Add conference detail

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Conference;
+use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -11,16 +13,50 @@ use Twig\Environment;
 
 class ConferenceController extends AbstractController
 {
+
+    private ConferenceRepository $conferenceRepository;
+    private CommentRepository $commentRepository;
+    private Environment $twig;
+
+    public function __construct(
+        ConferenceRepository $conferenceRepository,
+        CommentRepository $commentRepository, 
+        Environment $twig
+    ) {
+        $this->conferenceRepository = $conferenceRepository;
+        $this->commentRepository = $commentRepository;
+        $this->twig = $twig;
+    }
+
     /**
      * @Route("/", name="homepage")
      */
-    public function index(Environment $twig, ConferenceRepository $conferenceRepository): Response
+    public function index(): Response
     {
-
-        $conferences = $conferenceRepository->findAll();
+        $conferences = $this->conferenceRepository->findAll();
 
         return new Response(
-            $twig->render('conference/index.html.twig', ['conferences' => $conferences])
+            $this->twig->render('conference/index.html.twig', ['conferences' => $conferences])
         );
+    }
+
+    /**
+     * @Route("/conference/{id}", name="conference")
+     */
+    public function show(int $id): Response
+    {
+        $conference = $this->conferenceRepository->findOneBy(['id' => $id ]);
+        $comments = $this->commentRepository->findBy(
+            ['conference' => $conference], 
+            ['createdAt' => 'DESC']
+        );
+        
+        return new Response(
+            $this->twig->render('conference/show.html.twig', [
+                'conference' => $conference,
+                'comments' => $comments
+            ])
+        );
+
     }
 }
