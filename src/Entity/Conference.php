cf283657ea6333698cb2cfbdd6daaed3a67894cf
Francisco Marcet <fcomarcet1@gmail.com>
2021-09-18 18:28:33 +0200

    add method for generate slugs

--- src/Entity/Conference.php
+++ src/Entity/Conference.php
@@ -2,10 +2,11 @@
 
 namespace App\Entity;
 
-use App\Repository\ConferenceRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\ConferenceRepository;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 
@@ -54,6 +55,12 @@ class Conference
         $this->comments = new ArrayCollection();
     }
 
+    public function computeSlug(SluggerInterface $slugger)
+    {
+        if (!$this->slug || '-' === $this->slug) {
+            $this->slug = (string) $slugger->slug((string) $this)->lower();
+        }
+    }
 
     public function __toString()
     {
