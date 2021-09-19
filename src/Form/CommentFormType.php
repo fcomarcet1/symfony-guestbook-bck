3a3213612daa813e8bdd174082269f1a5030feb1
Francisco Marcet <fcomarcet1@gmail.com>
2021-09-19 09:29:16 +0200

    Customizing CommentFormType

--- src/Form/CommentFormType.php
+++ src/Form/CommentFormType.php
@@ -5,20 +5,39 @@ namespace App\Form;
 use App\Entity\Comment;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Image;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class CommentFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('author')
-            ->add('text')
-            ->add('email')
-            ->add('createdAt')
-            ->add('photoFilename')
-            ->add('conference')
-        ;
+            ->add('author', TextType::class, [
+                'label' => 'Your name',
+                'required' => true
+            ])
+            ->add('text', TextareaType::class, [
+                'label' => 'Message: ',
+                'required' => true
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email: ',
+                'required' => true
+            ])
+            ->add('photo', FileType::class, [
+                'required' => false,
+                'mapped' => false,
+                'constraints' => [
+                    new Image(['maxSize' => '1024k'])
+                ],
+            ])
+            ->add('submit', SubmitType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver)
