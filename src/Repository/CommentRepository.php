e4b984745c09373010f9a92fcbd1c7b80241709b
fcomarcet1 <fcomarcet1@gmail.com>
2021-10-06 08:36:58 +0200

    Update CommentRepository for publish only comments with state = published

--- src/Repository/CommentRepository.php
+++ src/Repository/CommentRepository.php
@@ -18,17 +18,27 @@ use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 class CommentRepository extends ServiceEntityRepository
 {
     public const PAGINATOR_PER_PAGE = 2;
+    private const COMMENT_STATE_PUBLISHED = 'published';
 
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Comment::class);
     }
 
+    /**
+     * Get list of all comments paginates where state is published
+     *
+     * @param Conference $conference
+     * @param integer $offset
+     * @return Paginator
+     */
     public function getCommentPaginator(Conference $conference, int $offset): Paginator
     {
         $query = $this->createQueryBuilder('c')
             ->andWhere('c.conference = :conference')
+            ->andWhere('c.state = :state')
             ->setParameter('conference', $conference)
+            ->setParameter('state', self::COMMENT_STATE_PUBLISHED)
             ->orderBy('c.createdAt', 'DESC')
             ->setMaxResults(self::PAGINATOR_PER_PAGE)
             ->setFirstResult($offset)
